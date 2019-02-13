8c39ee4561c22b984bd231db03fb08a86f33d30a
joningi98 <joningi17201@gmail.com>
2019-02-13 19:32:05 +0000

    timi

---
+++ Timi/Timi_8/double-ended_queue.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data=None, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return str(self.data)
+
+
+class DLL_Deque(object):
+    def __init__(self):
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.size = 0
+
+    def push_front(self, data):
+        new_node = Node(data, self.head.next, self.head)
+        self.head.next = new_node
+        new_node.next.prev = new_node
+        self.size += 1
+
+    def push_back(self, data):
+        new_node = Node(data, self.tail, self.tail.prev)
+        new_node.prev.next = new_node
+        self.tail.prev = new_node
+
+        self.size += 1
+
+    def __str__(self):
+        ret_str = ""
+        node = self.head.next
+        while node.next is not None:
+            ret_str += str(node.data) + "\n"
+            node = node.next
+        return ret_str
+
+
+dl_list = DLL_Deque()
+
+dl_list.push_front(1)
+dl_list.push_front(2)
+dl_list.push_front(3)
+dl_list.push_back(56)
+
+
+print(dl_list)
+
+
+
