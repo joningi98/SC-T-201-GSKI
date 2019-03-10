7f0199190af8ec85dc3e4bbbb891b09b4dbd806f
joningi98 <joningi17201@gmail.com>
2019-03-10 22:50:06 +0000

    pa4

--- PA/PA_4/ADT_Bucket.py
+++ PA/PA_4/ADT_Bucket.py
@@ -1,3 +1,14 @@
+import random
+
+
+class NotFoundException(Exception):
+    pass
+
+
+class ItemExistsException(Exception):
+    pass
+
+
 class Node:
     def __init__(self, key=None, data=None, next=None):
         self.key = key
@@ -15,19 +26,13 @@ class Bucket:
         self.size = 0
 
     def insert(self, key, data):
-        node = self.head
-        change = False
-        while node is not None:
-            if str(node.key) == str(key):
-                node.data = data
-                change = True
-                break
-            node = node.next
-        if not change:
+        if not self.contains(key):
             self.head = Node(key, data, self.head)
             if self.tail is None:
                 self.tail = self.head
             self.size += 1
+        else:
+            raise ItemExistsException()
 
     def update(self, key, data):
         try:
@@ -37,10 +42,8 @@ class Bucket:
 
     def find(self, key):
         if self.contains(key):
-            _, node = self.contains(key)
-            return node.data
-        else:
-            print("not found")
+            return self[key]
+        # raise NotFoundException()
 
     def contains(self, key):
         return key in self
@@ -49,21 +52,37 @@ class Bucket:
         node = self.head
         while node is not None:
             if str(node.key) == str(key):
-                return True, node
+                return True
             node = node.next
         return False
 
     def remove(self, key):
-        pass
+        if self.contains(key):
+            node_1 = self.head
+            node_2 = self.head.next
+            while node_2 is not None:
+                if str(node_1.key) == str(key):
+                    self.head = node_1.next
+                    self.size -= 1
+                    break
+                if str(node_2.key) == str(key):
+                    node_1.next = node_2.next
+                    self.size -= 1
+                    break
+                node_1 = node_1.next
+                node_2 = node_2.next
+        else:
+            pass
+            # raise NotFoundError()
 
     def __setitem__(self, key, data):
-        try:
-            node = self.head
+        node = self.head
+        if self.contains(key):
             while node is not None:
                 if str(node.key) == str(key):
                     node.data = data
                 node = node.next
-        except IndexError:
+        else:
             self.insert(key, data)
 
     def __getitem__(self, key):
@@ -72,7 +91,7 @@ class Bucket:
             if str(node.key) == str(key):
                 return node.data
             node = node.next
-        return IndexError
+        # raise NotFound error
 
     def __len__(self):
         return self.size
@@ -89,13 +108,18 @@ class Bucket:
         return ret_str
 
 
-class Map:
+class HashMap:
     def __init__(self):
         self.map = [Bucket() for _ in range(10)]
-        self.size = 0
+        self.size = 10
 
     def insert(self, key, data):
-        pass
+        selected_bucket = self.map[key]
+        if selected_bucket.contains(key):
+            raise ItemExistsException
+            pass
+        else:
+            selected_bucket.insert(key, data)
 
     def update(self, key, data):
         pass
@@ -110,27 +134,63 @@ class Map:
         pass
 
     def __setitem__(self, key, data):
-        pass
+        try:
+            chose_bucket = self.map[key]
+            chose_bucket[key] = data
+        except IndexError:
+            pass
 
     def __getitem__(self, key):
-        pass
+        try:
+            chose_bucket = self.map[key]
+            return chose_bucket[key]
+        except IndexError:
+            pass
 
     def __len__(self):
+        return self.size
+
+    def __str__(self):
+        ret_str = ""
+        for ix, bucket in enumerate(self.map):
+            ret_str += "Bucket :{}, [{}]\n".format(ix, bucket)
+        return ret_str
+
+
+class MyHashableKey:
+    def __init__(self, int_value=0, string_value=""):
+        self.int = int_value
+        self.str = string_value
+
+    def __eq__(self, other):
+        return self.int == other.int and self.str == other.str
+
+
+    def __hash__(self):
         pass
 
-
+'''
 if __name__ == "__main__":
+    my_map = HashMap()
+    my_map.insert(1, "A")
+    my_map.insert(2, "B")
+    my_map.insert(3, "C")
+    my_map.insert(4, "D")
+    my_map.insert(5, "E")
+    my_map.insert(6, "F")
+    my_map.insert(7, "G")
+    my_map.insert(8, "H")
+    print(my_map)
+'''
+
+'''
+if __name__ == "__main__":
+    print("Testing bucket")
     my_b = Bucket()
     my_b.insert(1, "A")
     my_b.insert(2, "B")
     my_b.insert(3, "C")
+    my_b[4] = 'Bucket'
     print(my_b)
-    my_b[1] = "Hello"
-    print(my_b)
-    print(my_b[2])
-    my_b[2] = "Valli"
-    print(my_b)
-    my_b.update(4, "Jón")
-    print(my_b)
-    print(1 in my_b)
-    print(my_b.find(3))
+'''
+
--- Timi/TImi_16/hash_code.py
+++ Timi/TImi_16/hash_code.py
@@ -2,9 +2,9 @@ from random import Random
 
 
 class Key:
-    def __init__(self, int_value, int_value2):
+    def __init__(self, int_value, string_value):
         self.i = int_value
-        self.i2 = int_value2
+        self.i2 = string_value
 
     def __eq__(self, other):
         return self.i == other.i and self.i2 == other.i2
@@ -13,12 +13,25 @@ class Key:
         return self.i * self.i2
 
 
+def find_prime(size_of_lis):
+    my_list = []
+    for num in range(2, size_of_lis):
+        prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                prime = False
+        if prime:
+            my_list.append(num)
+    return my_list[rand.randint(0, len(my_list) - 1)]
+
+
 if __name__ == "__main__":
     rand = Random()
     lis_size = 8
     lis = [0] * lis_size
     for _ in range(1000):
-        key = Key(rand.randint(0, 100), rand.randint(0, 100))
+        the_prime_number = find_prime(lis_size)
+        key = Key(rand.randint(0, 100), "string")
         index = hash(key) % lis_size
         lis[index] += 1
 
